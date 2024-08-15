3b4bd34d195cbf2db5cde26bdaed1bb89a4280bd
santiago cortes tovar <scortua@hotmail.com>
2024-08-15 17:40:35 -0500

    Hecha el prototipo del test con errores en igualar valores

--- tercio 1/complexNumbers/test.py
+++ tercio 1/complexNumbers/test.py
@@ -1,42 +1,60 @@
 import unittest
-from complejos import *
+from libraryComplex import *
 
 class TestComplexNumbers(unittest.TestCase):
-    def sumaTest(self):
-        a,b = [1,2],[3,4]
-        self.assertEqual(sumaC(a,b),(4,6))
+    def testSuma(self):
+        a,b = [1,2], [3,4]
+        c,d = [5,10], [3.5,9.1]
+        self.assertAlmostEqual(sumaC(a,b), [4,6])
+        self.assertAlmostEqual(sumaC(c,d), [8.5,19.1])
 
-    def productoTest(self):
-        a,b = [1,2],[3,4]
-        self.assertEqual(productoC(a,b),(-5,10))
+    def testProducto(self):
+        a,b = [1,2], [3,4]
+        c,d = [5,10], [3.5,9.1]
+        self.assertAlmostEqual(productoC(a,b), [-5,10])
+        self.assertAlmostEqual(productoC(c,d), [-77.5,55.5])
 
-    def restaTest(self):
-        a,b = [1,2],[3,4]
-        self.assertEqual(restaC(a,b),(-2,-2))
+    def testResta(self):
+        a,b = [1,2], [3,4]
+        c,d = [5,10], [3.5,9.1]
+        self.assertAlmostEqual(restaC(a,b), [-2,-2])
+        self.assertAlmostEqual(restaC(c,d), [1.5,0.9])
     
-    def divisionTest(self):
-        a,b = [1,2],[3,4]
-        self.assertEqual(divisionC(a,b),(0.44,0.08))
+    def testDivision(self):
+        a,b = [1,2], [3,4]
+        c,d = [5,10], [3.5,9.1]
+        self.assertAlmostEqual(divisionC(a,b), [0.44,0.08])
+        self.assertAlmostEqual(divisionC(c,d), [0.85,0.05])
 
-    def moduloTest(self):
+    def testModulo(self):
         a = [3,4]
-        self.assertEqual(moduloC(a),5)
+        d = [3.5,9.1]
+        self.assertAlmostEqual(moduloC(a), 5)
+        self.assertAlmostEqual(moduloC(d), 9.74)
     
-    def conjugadoTest(self):
+    def testConjugado(self):
         a = [3,4]
-        self.assertEqual(conjugadoC(a),(3,-4))
+        d = [3.5,9.1]
+        self.assertAlmostEqual(conjugadoC(a), [3,-4])
+        self.assertEqual(conjugadoC(d), [3.5,-9.1])  
     
-    def polarACartesianaTest(self):
-        r,p = 5,pi/2
-        self.assertEqual(polarACartesiana(r,p),(0,5))
+    def testPolarACartesiana(self):
+        r1,p1 = 5, pi/2
+        r2,p2 = 2, pi/4
+        self.assertAlmostEqual(polarACartesiana(r1,p1), [0,5])
+        self.assertAlmostEqual(polarACartesiana(r2,p2), [1.41,1.41])
     
-    def cartesianoAPolarTest(self):
+    def testcartesianoAPolar(self):
         a = [3,4]
-        self.assertEqual(cartesianoAPolar(a),(5,0.93))
+        d = [3.5,9.1]
+        self.assertAlmostEqual(cartesianoAPolar(a), [5,0.92])
+        self.assertAlmostEqual(cartesianoAPolar(d), [9.5,1.19])
     
-    def faseTest(self):
+    def testFase(self):
         a = [3,4]
-        self.assertEqual(faseC(a),0.93)
+        d = [3.5,9.1]
+        self.assertAlmostEqual(faseC(a), 0.92)
+        self.assertAlmostEqual(faseC(d), 1.20)
 
 if __name__ == '__main__':
     unittest.main()
